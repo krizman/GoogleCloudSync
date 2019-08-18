006dfc8a3fa089bdc51dba0f56ea5d789594ef5f
Uroš Križman <uros.krizman@gmail.com>
2019-08-18 21:43:42 +0200

    Added Google Cloud Client package with dependencies and used it to upload newly created file to the bucket

--- GoogleCloudSync/CloudManager.cs
+++ GoogleCloudSync/CloudManager.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Storage.V1;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -10,6 +12,8 @@ namespace GoogleCloudSync
     public class CloudManager
     {
         private readonly FileSystemWatcher fileSystemWatcher;
+        // TODO: add to config or something
+        private const string bucketName = "login5-agro-files-source-1566127485";
 
         public CloudManager(
             string pathToWatch, 
@@ -17,10 +21,8 @@ namespace GoogleCloudSync
             bool includeSubdirectories = false)
         {
             fileFilter = string.IsNullOrWhiteSpace(fileFilter) ? "*.*" : fileFilter;
+
             fileSystemWatcher = new FileSystemWatcher(pathToWatch, fileFilter);
-            fileSystemWatcher.Created += FileSystemWatcher_Created;
-            //fileSystemWatcher.Renamed += FileSystemWatcher_Renamed;
-            //fileSystemWatcher.Changed += FileSystemWatcher_Changed;
             fileSystemWatcher.IncludeSubdirectories = includeSubdirectories;
             fileSystemWatcher.NotifyFilter =
                 NotifyFilters.CreationTime
@@ -29,22 +31,40 @@ namespace GoogleCloudSync
                 | NotifyFilters.LastAccess
                 | NotifyFilters.LastWrite;
 
+            fileSystemWatcher.Created += FileSystemWatcher_Created;
+
             fileSystemWatcher.EnableRaisingEvents = true;
-        }
-
-        private void FileSystemWatcher_Changed(object sender, FileSystemEventArgs e)
-        {
-            Console.WriteLine($"{nameof(FileSystemWatcher_Changed)} File {e.FullPath} was {e.ChangeType}");
-        }
-
-        private void FileSystemWatcher_Renamed(object sender, RenamedEventArgs e)
-        {
-            Console.WriteLine($"{nameof(FileSystemWatcher_Renamed)} File {e.FullPath} was {e.ChangeType}");
         }
 
         private void FileSystemWatcher_Created(object sender, FileSystemEventArgs e)
         {
             Console.WriteLine($"{nameof(FileSystemWatcher_Created)} File {e.FullPath} was {e.ChangeType}");
+
+            UploadToCloud(e.FullPath);
+        }
+
+        private void UploadToCloud(string filePath)
+        { 
+
+            // Explicitly use service account credentials by specifying the private key file.
+            // The service account should have Object Manage permissions for the bucket.
+            GoogleCredential credential = GoogleCredential.FromFile("credentials.json");
+
+            StorageClient storageClient = StorageClient.Create(credential);
+
+            // TODO: handle file errors and GC upload errors
+            using (var fileStream = new FileStream(
+                filePath, 
+                FileMode.Open,
+                FileAccess.Read, 
+                FileShare.Read))
+            {
+                Console.WriteLine($"Uploading file '{filePath}' to bucket '{bucketName}' ...");
+
+                storageClient.UploadObject(bucketName, Path.GetFileName(filePath), "text/csv", fileStream);
+
+                Console.WriteLine("Upload complete.");
+            }
         }
     }
 }
