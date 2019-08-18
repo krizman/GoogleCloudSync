9b981615709510debac8291e4a80033d2177a4f7
Uroš Križman <uros.krizman@gmail.com>
2019-08-18 19:03:50 +0200

    initial commit with working FileSystemWatcher implementation and ConsoleApp launcher

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using GoogleCloudSync;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = args.ElementAtOrDefault(0);
+            string filter = args.ElementAtOrDefault(1);
+            bool.TryParse(args.ElementAtOrDefault(2), out bool includeSubdir);
+            new CloudManager(path, filter, includeSubdir);
+
+            Console.WriteLine($"Listening for new files on path '{path}' (include subdirs={includeSubdir}) with filter '{filter}'");
+            Console.WriteLine("Press any key to stop...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GoogleCloudSync/CloudManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleCloudSync
+{
+    public class CloudManager
+    {
+        private readonly FileSystemWatcher fileSystemWatcher;
+
+        public CloudManager(
+            string pathToWatch, 
+            string fileFilter = null, 
+            bool includeSubdirectories = false)
+        {
+            fileFilter = string.IsNullOrWhiteSpace(fileFilter) ? "*.*" : fileFilter;
+            fileSystemWatcher = new FileSystemWatcher(pathToWatch, fileFilter);
+            fileSystemWatcher.Created += FileSystemWatcher_Created;
+            //fileSystemWatcher.Renamed += FileSystemWatcher_Renamed;
+            //fileSystemWatcher.Changed += FileSystemWatcher_Changed;
+            fileSystemWatcher.IncludeSubdirectories = includeSubdirectories;
+            fileSystemWatcher.NotifyFilter =
+                NotifyFilters.CreationTime
+                | NotifyFilters.DirectoryName
+                | NotifyFilters.FileName
+                | NotifyFilters.LastAccess
+                | NotifyFilters.LastWrite;
+
+            fileSystemWatcher.EnableRaisingEvents = true;
+        }
+
+        private void FileSystemWatcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"{nameof(FileSystemWatcher_Changed)} File {e.FullPath} was {e.ChangeType}");
+        }
+
+        private void FileSystemWatcher_Renamed(object sender, RenamedEventArgs e)
+        {
+            Console.WriteLine($"{nameof(FileSystemWatcher_Renamed)} File {e.FullPath} was {e.ChangeType}");
+        }
+
+        private void FileSystemWatcher_Created(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"{nameof(FileSystemWatcher_Created)} File {e.FullPath} was {e.ChangeType}");
+        }
+    }
+}
